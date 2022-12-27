092da809340b9707ce0138f9544c71d15deff4c6
Hilton1 <josehilton02@gmail.com>
2022-12-27 19:24:41 -0300

    Tipando orders e renderizando lista de pedidos

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,5 +1,34 @@
 import { Container } from './styles';
 import { OrdersBoard } from '../OrdersBoards';
+import { Order } from '../../types/Order';
+
+const orders: Order[]= [
+  {
+    '_id': '63ab2fc3d29afdc35ccf77a6',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1672153689059-quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '63ab2fc3d29afdc35ccf77a7'
+      },
+      {
+        'product': {
+          'name': 'Coca-cola',
+          'imagePath': '1672162416988-coca-cola.png',
+          'price': 7,
+        },
+        'quantity': 2,
+        '_id': '63ab2fc3d29afdc35ccf77a8'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
@@ -7,14 +36,17 @@ export function Orders() {
       <OrdersBoard
         icon = "🕑"
         title = "Fila de espera"
+        orders = {orders}
       />
       <OrdersBoard
         icon = "👨‍🍳"
         title = "Em preparação"
+        orders = {[]}
       />
       <OrdersBoard
         icon = "✅"
         title = "Pronto!"
+        orders = {[]}
       />
     </Container>
   );
