086fd11809142645148354af17683927faff357a
Hilton1 <josehilton02@gmail.com>
2023-01-05 00:15:09 -0300

    Criando OrderModal

---
+++ src/components/OrderModal/index.tsx
@@ -0,0 +1,19 @@
+import { Overlay, ModalBody } from './styles';
+
+interface OrderModalProps {
+  visible: boolean;
+}
+
+export function OrderModal({ visible }: OrderModalProps) {
+  if(!visible) {
+    return null;
+  }
+
+  return (
+    <Overlay>
+      <ModalBody>
+        Irruuuuu
+      </ModalBody>
+    </Overlay>
+  );
+}
