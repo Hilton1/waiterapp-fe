356ddabc6b514e2c3db14839eb6e9837d9613812
Hilton1 <josehilton02@gmail.com>
2023-02-28 00:30:28 -0300

    Deixando botões de status dinâmicos

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -108,8 +108,14 @@ export function OrderModal({
               disabled={isLoading}
               onClick={onChangeOrderStatus}
             >
-              <span>👨‍🍳</span>
-              <strong>Iniciar Produção</strong>
+              <span>
+                {order.status === 'WAITING' && '👨‍🍳'}
+                {order.status === 'IN_PRODUCTION' && '✅'}
+              </span>
+              <strong>
+                {order.status === 'WAITING' && 'Iniciar Produção'}
+                {order.status === 'IN_PRODUCTION' && 'Concluir Pedido'}
+              </strong>
             </button>
           )}
 
